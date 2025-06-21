4bf7329cd91175deae78e6eb0d77a2258e11c5c5
Zokyn <guttycasaes@hotmail.com>
2025-06-21 00:50:13 -0300

    views: alterado o retorno das views para JsonResponses

--- backend/doctor_pet_app/views.py
+++ backend/doctor_pet_app/views.py
@@ -1,21 +1,46 @@
-from django.shortcuts import render
+import json
+from .models import *
 from django.views.csrf import csrf_failure
-from django.http import HttpResponse, HttpRequest
+from django.http import JsonResponse, HttpResponse, HttpRequest
+from django.core.serializers import serialize
 
 def index (request: HttpRequest) -> HttpResponse:
-    return HttpResponse('home');
+    return HttpResponse('home')
 
 def tasks(request: HttpRequest) -> HttpResponse:
-    response = 'List of Tasks'
-    return HttpResponse(response)
+    response = Task.objects.all()
+    return JsonResponse(serialize('python', response), safe=False)
 
 def task_details(request: HttpRequest, task_id: int):
-    return HttpResponse('Task %s id Details' % task_id)
+    modelname = 'Task'
+    try :
+        response = Task.objects.get(pk=task_id)
+        data = {
+            'id': response.pk,
+            'title' : response.title,
+            'slug' : response.slug,
+            'specialty': response.specialty
+        }
+        return JsonResponse({'data': data})
+    except Task.DoesNotExist:
+        return JsonResponse({'error': f'{modelname} não encontrado'}, status=404)
 
 def task_attendances(request: HttpRequest, task_id: int):
-    response = 'List of attendances of Task %s' % task_id
-    return HttpResponse(response)
+    try :
+        response = Attendance.objects.filter(task_id=task_id)
+        data = [
+            {
+                **item['fields'],
+                'id': item['pk']
+            } for item in serialize('python', response)
+        ]
+        
+        return JsonResponse(data, safe=False)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
 
-def create_attendance(request: HttpRequest, task_id: int):
-    response = 'Creating attendance for Task %s'
-    return HttpResponse(response)
+
+# def create_attendance(request: HttpRequest, task_id: int):
+#     response = 'Creating attendance for Task %s'
+#     return HttpResponse(response)
+
